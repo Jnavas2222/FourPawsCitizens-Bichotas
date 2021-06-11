4f7be209e72b180e1de2eb92c6b908038631a8c6
arnunez2002 <arnunez@unbosque.edu.co>
2021-06-10 20:54:15 -0500

    nuevos cambios en repositories

---
+++ src/main/java/co/edu/unbosque/FourPawsCitizens_Bichotas/jpa/repositories/OfficerRepository.java
@@ -0,0 +1,14 @@
+package co.edu.unbosque.FourPawsCitizens_Bichotas.jpa.repositories;
+
+import co.edu.unbosque.FourPawsCitizens_Bichotas.jpa.entities.Official;
+import co.edu.unbosque.FourPawsCitizens_Bichotas.jpa.entities.Owner;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface OfficerRepository {
+
+    Optional<Official> save(Official official);
+
+    List<Owner> findAll();
+}
---
+++ src/main/java/co/edu/unbosque/FourPawsCitizens_Bichotas/jpa/repositories/OfficerRepositoryImpl.java
@@ -0,0 +1,29 @@
+package co.edu.unbosque.FourPawsCitizens_Bichotas.jpa.repositories;
+
+import co.edu.unbosque.FourPawsCitizens_Bichotas.jpa.entities.Official;
+
+import javax.persistence.EntityManager;
+import java.util.Optional;
+
+public class OfficerRepositoryImpl  implements OfficerRepository{
+
+
+    private EntityManager entityManager;
+
+    public OfficerRepositoryImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    public Optional<Official> save(Official official) {
+        try {
+            entityManager.getTransaction().begin();
+            entityManager.persist(official);
+            entityManager.getTransaction().commit();
+            return Optional.of(official);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return Optional.empty();
+    }
+}
